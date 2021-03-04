265bac559e093af93e4aad4f4f49b43abecc51be
snihurenko <snigurenko.anastasiya@gmail.com>
2021-03-04 22:48:09 +0200

    timer fixed

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -2,38 +2,42 @@ import React, { useState, useEffect } from 'react';
 import cn from 'classnames';
 import css from './Timer.module.scss';
 import moment from 'moment';
-
 interface TimerProps {
-  time: number; //time to countdown in seconds
-  autostart?: boolean; //If true should start immediately (use useEffect here)
-  step: number; //in ms— time to refresh timer in ms (time between ticks)
-  onTick: () => void; //executed every 'tick' (every interval)
-  onTimeEnd: () => void; //execute when time is gone
-  onTimeStart: () => void; //execute when the timer is started
-  onTimePause: () => void; //execute when the timer is stopped
+  time: number;
+  autostart?: boolean;
+  step: number;
+  onTick: () => void;
+  onTimeEnd: () => void;
+  onTimeStart: () => void;
+  onTimePause: () => void;
 }
 
 export const Timer = ({ time, autostart, step, onTick, onTimeEnd, onTimeStart, onTimePause }: TimerProps) => {
   const [currentTime, setCurrentTime] = useState<number>(time);
   const [timerState, setTimerState] = useState<boolean>(false);
-  const [intervalTimer, setIntervalTimer] = useState<number>(0);
+  const [intervalTimer, setIntervalTimer] = useState<NodeJS.Timeout>();
 
   const startTimer = (timeLeft: number) => {
-    // if (onTimeStart) {onTimeStart()};
+    if (onTimeStart) {
+      onTimeStart();
+    }
     setTimerState(true);
 
     const interval = setInterval(() => {
-      // if (onTick) {onTick()};
+      if (onTick) {
+        onTick();
+      }
       timeLeft--;
       setCurrentTime(timeLeft);
 
       if (timeLeft <= 0) {
-        clearInterval(intervalTimer);
+        clearInterval(intervalTimer!);
         setTimerState(false);
-        // if (onTimeEnd) {onTimeEnd()};
+        if (onTimeEnd) {
+          onTimeEnd();
+        }
       }
     }, step);
-
     setIntervalTimer(interval);
   };
 
@@ -42,9 +46,7 @@ export const Timer = ({ time, autostart, step, onTick, onTimeEnd, onTimeStart, o
       onTimePause();
     }
     setTimerState(false);
-    console.log('intervalTimer', intervalTimer);
-
-    clearInterval(intervalTimer);
+    clearInterval(intervalTimer!);
   };
 
   useEffect(() => {
