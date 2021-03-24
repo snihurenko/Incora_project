075c99010e3c944570e6e8e9613100594f4867ab
snihurenko <snigurenko.anastasiya@gmail.com>
2021-03-24 15:05:54 +0200

    fix

--- src/utils/validation_schemas.ts
+++ src/utils/validation_schemas.ts
@@ -9,10 +9,10 @@ export const paymentFormSchema = Yup.object().shape({
     .min(3, 'Minimum 3 symbols')
     .max(15, 'Maximum 15 symbols')
     .required('Valid last name is required.'),
-  userName: Yup.string()
+  username: Yup.string()
     .min(3, 'Minimum 3 symbols')
     .max(15, 'Maximum 15 symbols')
-    .required('VYour username is required.'),
+    .required('Your username is required.'),
   email: Yup.string()
     .email('Invalid email')
     .required('Please enter valid email address for shipping updates'),
@@ -21,8 +21,8 @@ export const paymentFormSchema = Yup.object().shape({
     .max(64, 'Maximum 64 symbols')
     .required('Please enter your shipping address'),
   address2: Yup.string().min(5, 'Minimum 5 symbols').max(64, 'Maximum 64 symbols').optional(),
+  country: Yup.string().required('Please select a valid country.'),
   state: Yup.string().min(3, 'Minimum 3 symbols').max(24, 'Maximum 24 symbols').optional(),
   zip: Yup.string().length(5, 'Should have 5 symbols').required('Zip code is required'),
-  country: Yup.string().required('Please select a valid country.'),
   payment: Yup.string().required('Please choose payment method')
 });
